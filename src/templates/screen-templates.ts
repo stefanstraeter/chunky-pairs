2d9ae2893f9660694dd4aab8941808abe6761ea0
Stefan Sträter <stefan.straeter@gmail.com>
2026-07-02 14:35:36 +0200

    refactor: update game screen template with player widget IDs and adjust timer countdown

--- src/templates/screen-templates.ts
+++ src/templates/screen-templates.ts
@@ -108,13 +108,18 @@ export function createMatchSetupScreenTemplate(): string {
     TEMPLATE GAME SCREEN
     ========================================================================== */
 
+/**
+ * @description Creates the game screen template.
+ * @export
+ * @return {string}  The HTML string for the game screen.
+ */
 export function createGameScreenTemplate(): string {
   return `
     <section id="screen-game" class="screen game">
       <header id="game-header" class="game__header"></header>
       <div class="game__main-layout">
 
-        <div class="player-card player-card--1">
+        <div id="widget-player-1" class="player-card player-card--1">
           <img class="player-card__avatar" src="${assetPath("img/00_general/player-one.svg")}" alt="Player 1 Avatar" />
           <span class="player-card__label">PLAYER 1</span>
           <span id="score-player-1" class="player-card__score">0</span>
@@ -127,12 +132,12 @@ export function createGameScreenTemplate(): string {
             <img id="turn-timer-bg" class="turn-timer__bg" src="${assetPath("img/00_general/turn-background-red.svg")}" alt="">
             <div class="turn-timer__content">
               <span id="turn-timer-label" class="turn-timer__player">PLAYER 1'S TURN</span>
-              <span id="turn-timer-countdown" class="turn-timer__time">30s</span>
+              <span id="turn-timer-countdown" class="turn-timer__time">5s</span>
             </div>
           </div>
         </div>
 
-        <div class="player-card player-card--2">
+        <div id="widget-player-2" class="player-card player-card--2">
           <img class="player-card__avatar" src="${assetPath("img/00_general/player-two.svg")}" alt="Player 2 Avatar" />
           <span class="player-card__label">PLAYER 2</span>
           <span id="score-player-2" class="player-card__score">0</span>
