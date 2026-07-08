b5e50d9524db328d536da96e7bb57888ab944f54
Stefan Sträter <stefan.straeter@gmail.com>
2026-07-08 15:05:37 +0200

    fix: add passive event listener for touchstart to improve performance

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,4 @@ import { showHomeScreen, showResultScreen } from "./ui/navigation";
  * The entry point of the application. It initializes the user interface once the DOM content is fully loaded.
  */
 document.addEventListener("DOMContentLoaded", showHomeScreen);
+document.addEventListener("touchstart", () => {}, { passive: true });
